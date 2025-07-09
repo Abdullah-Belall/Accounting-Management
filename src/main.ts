7cf90c651172fceb25dfafc0b62cba9a880ad0f6
Abdullah-Belall <abdullahbelal102938@gmail.com>
2025-07-09 12:39:36 +0300

    o

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: false,
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: false,
   });
   await app.listen(process.env.PORT ?? 4000);
 }
