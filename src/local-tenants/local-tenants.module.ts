fd95299f64c4ebfd2364aa7814d07917109af697
Abdullah-Belall <abdullahbelal102938@gmail.com>
2025-07-09 07:22:57 +0300

    k

--- src/local-tenants/local-tenants.module.ts
+++ src/local-tenants/local-tenants.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { LocalTenantsService } from './local-tenants.service';
 import { LocalTenantsController } from './local-tenants.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { LocalTenantEntity } from './entities/local-tenant.entity';
+import { LocalTenantsEntity } from './entities/local-tenant.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LocalTenantEntity])],
+  imports: [TypeOrmModule.forFeature([LocalTenantsEntity])],
   controllers: [LocalTenantsController],
   providers: [LocalTenantsService],
 })
